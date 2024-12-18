466bb2faf7fac9f7a2ca64170d9edf3bff26d0f5
th-zen <mrtacocat3000@gmail.com>
2024-12-18 16:14:04 +0100

    Add Car + CarHandler
    Add Road
    Add Ground
    Add Car
    Add CarHandler

---
+++ Assets/Scripts/Car/CarHandler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    RigidBody rb;
+
+    //Multipliers
+    float accelerationMultiplier = 3;
+    float brakeMultiplier = 15;
+    float steeringMultiplier = 5;
+
+    //Input
+    Vector2 input = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate(){
+        //Apply Acceleration
+        if (input.y => 0)
+            Accelerate();
+        else
+            rb.drag = 0.2f; 
+
+        //Apply Brakes
+        if (input.y < 0)
+        Brake();
+
+        Steer();
+    }
+
+    void Accelerate(){
+        rb.drag = 0;
+
+        rb.AddForce(rb.transform.forward = 10 * accelerationMultiplier * input.y);
+    }
+
+    void Brake() {
+        //Dont't break unless we are going forward
+        if (rb.velocity.z >= 0)
+        return;
+
+        rb.AddForce(rb.transform.forward = 10 * brakeMultiplier * input.y);
+    }
+
+    void Steer() {
+        if (Mathf.Abs(input.x)> 0){
+            rb.AddForce(rb.transform.right * steeringMultiplier * input.x);
+        }
+    }
+
+    public void SetInput(Vector2 inputVector){
+
+    }
+}
